9ff32c06cb398afdce6066e6b7c589887eeffad8
Chris Bull <chris@chrisbull.com>
2021-05-21 02:23:21 -0700

    convert to typescript

---
+++ src/pages/Blank.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageTitle from '../components/Typography/PageTitle';
+function Blank() {
+  return (
+    <>
+      <PageTitle>Blank</PageTitle>
+    </>
+  );
+}
+export default Blank;
